b861f18a43608ffd5da23849ad6172a171ff18ed
ErickNK <erick.kirabui@gmail.com>
2022-09-09 18:06:50 +0300

    initial commit

---
+++ src/main/java/com/flycode/lendingandrepaymentservice/LendingAndRepaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flycode.lendingandrepaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingAndRepaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingAndRepaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flycode/lendingandrepaymentservice/LendingAndRepaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flycode.lendingandrepaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingAndRepaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
